6a730080a507ddf9999fd528f38bf17c3ef7cd03
Dragan Janjic <janjic.drgn@gmail.com>
2021-01-26 17:42:07 +0100

    Added local storage save/load options, fixed css

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Todo from "./Todo";
 import TodoForm from "./TodoForm";
 
 function TodoList() {
   const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    loadTodos();
+  }, []);
+
+  useEffect(() => {
+    saveTodos();
+  }, [todos]);
+
+  const loadTodos = () => {
+    if (localStorage.getItem("todos" === null)) {
+      localStorage.setItem("todos", JSON.stringify(todos));
+      setTodos([]);
+    } else {
+      const data = JSON.parse(localStorage.getItem("todos"));
+      setTodos(data);
+    }
+  };
+
+  const saveTodos = () => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
 
   const addTodo = (todo) => {
     if (!todo.text) {
